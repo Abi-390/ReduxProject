c7e014d95dc7f256764acb3766963fae1d4e5bb6
Abi-390 <dekaabinash07@gmail.com>
2025-12-19 17:24:25 +0530

    tired of this debugging man

--- frontend/src/api/axiosconfig.jsx
+++ frontend/src/api/axiosconfig.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  baseURL: "https://otaku-backend-92hv.onrender.com",
 });
 
 export default instance;
